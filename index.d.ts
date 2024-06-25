86364725816900041a154c2f41dc0c6d315aa53b
Kristaps Picelēvičs <kristaps.picelevics@gmail.com>
2024-06-25 10:24:38 +0300

    Changed index file

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,5 @@ declare module 'screen-events' {
   // Define the types for the module here
   // Example:
   export function echo(param: string): void;
-  export function addListener(param: string): void;
+  export function isScreenOn(): boolean;
 }
