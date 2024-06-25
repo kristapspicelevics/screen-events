7874f9f96284c1ab282463c0a06b9f38f5e9c764
Kristaps Picelēvičs <kristaps.picelevics@gmail.com>
2024-06-25 10:30:30 +0300

    Removed async

--- src/web.ts
+++ src/web.ts
@@ -8,8 +8,8 @@ export class ScreenEventsWeb extends WebPlugin implements ScreenEventsPlugin {
     console.log('ECHO', options);
     return options;
   }
-
-  async isScreenOn(): Promise<{ result: boolean; }> {
+  
+  isScreenOn(): Promise<{ result: boolean; }> {
     throw new Error('Method not implemented.');
   }
 
