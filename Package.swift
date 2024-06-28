9357d338ee9aefc06eab90dbfe32806626c6739a
Kristaps Picelēvičs <kristaps.picelevics@gmail.com>
2024-06-28 15:33:05 +0300

    ios deployment target

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "ScreenEvents",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "ScreenEvents",
