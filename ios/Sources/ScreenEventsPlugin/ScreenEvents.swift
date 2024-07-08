7393710a2ebb358e8e9dbb76721ab1f7e0de35b5
Kristaps Picelēvičs <kristaps.picelevics@gmail.com>
2024-07-08 11:59:35 +0300

    Redon ScreenEvent.swift file

--- ios/Sources/ScreenEventsPlugin/ScreenEvents.swift
+++ ios/Sources/ScreenEventsPlugin/ScreenEvents.swift
@@ -6,29 +6,24 @@ import Foundation
         return value
     }
 
-    @objc public func getTotalScreenTime(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func getTotalScreenTime() -> String {
+        return
     }
 
-    @objc public func resetScreenTime(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func resetScreenTime() ->String {
+        return
     }
 
-    @objc public func start(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func start() -> Bool {
+        return
     }
 
-    @objc public func stop(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func stop() -> Bool {
+        return
     }
 
-    @objc public func isScreenOn(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func isScreenOn() -> Bool {
+        return
     }
 
     @objc public func screenDidConnect(_ value: String) -> String {
